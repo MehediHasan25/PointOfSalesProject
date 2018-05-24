fffaa6d69171d0d651e8c3c103133a7e067a8a6f
MehediHasan25 <ishanmehadi@gmail.com>
2018-05-24 17:06:11 +0600

    IncomeReport & IncomeReport details and alll classes  are completed

---
+++ POS.Models/EntityModel/IncomeReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Models.EntityModel
+{
+    public class IncomeReport
+    {
+        public int Id { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+
+        public int PurchaseReportId { get; set; }
+        public PurchaseReport PurchaseReport { get; set; }
+
+        public int SalesReportId { get; set; }
+        public SalesReport SalesReport { get; set; }
+
+        public List<IncomeReportDetails> IncomeReportDetailses { get; set; }
+
+    }
+}
