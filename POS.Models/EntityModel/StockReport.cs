8423aef9cd62d83c19f0b4d13bd5a9efdff4c93a
MehediHasan25 <ishanmehadi@gmail.com>
2018-05-24 16:46:31 +0600

    StockReport & StockReportDetails are added

---
+++ POS.Models/EntityModel/StockReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Models.EntityModel
+{
+    public class StockReport
+    {
+        public int Id { get; set; }
+
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+
+        public int OrganizationId { get; set; }
+        public Organizations Organization { get; set; }
+
+        public List<StockReportDetails>StockReportDetailses { get; set; }
+    }
+}
---
+++ POS.Models/EntityModel/StockReportDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Models.EntityModel
+{
+  public  class StockReportDetails
+    {
+        public int Id { get; set; }
+        public string CategoryFullPath { get; set; }
+        public double AveragePrice { get; set; }
+
+        public int StockId { get; set; }
+        public Stock Stock { get; set; }
+
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        public int StockReportId { get; set; }
+        public StockReport StockReport { get; set; }
+    }
+}
