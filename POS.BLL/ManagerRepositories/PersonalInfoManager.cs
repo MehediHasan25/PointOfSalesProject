300e75df6adc5af3be4d06604dc7513fca7c0076
Refat Bin Mustaque <refatmustaque@gmail.com>
2018-05-29 18:09:51 +0600

    ItemCategory table Namelist added

---
+++ POS.BLL/ManagerRepositories/PersonalInfoManager.cs
@@ -0,0 +1,19 @@
+﻿using POS.BLL.BaseManager;
+using POS.Models.EntityModel;
+using POS.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.BLL.ManagerRepositories
+{
+    public class PersonalInfoManager : Manager<PersonalInfo>
+    {
+        public PersonalInfoManager() : base(new PersonalInfoRepository())
+        {
+
+        }
+    }
+}
