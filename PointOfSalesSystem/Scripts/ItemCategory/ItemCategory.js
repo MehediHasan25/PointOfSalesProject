3c86254160709ca6f6c7e0b7c4da49ec0e0ce701
MehediHasan25 <ishanmehadi@gmail.com>
2018-05-31 03:12:21 +0600

    item category jquery work is continue

---
+++ PointOfSalesSystem/Scripts/ItemCategory/ItemCategory.js
@@ -0,0 +1,14 @@
+﻿
+
+
+$(document).ready(function () {
+    $(":radio:eq(0)").click(function () {
+        alert("The paragraph was clicked.");
+        //$("#Root").show(1000);
+    });
+
+    $(":radio:eq(1)").click(function () {
+        $("#cathide").hide(1000);
+    });
+
+});
