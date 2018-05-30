6de5aa80484d88c205166b50185d9d1a50b85819
MehediHasan25 <ishanmehadi@gmail.com>
2018-05-30 14:37:36 +0600

    ExpenseClass modified for view Design and Controller

---
+++ PointOfSalesSystem/Controllers/EmployeeInfoController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using PointOfSalesSystem.Models.ViewModels;
+using POS.BLL.ManagerRepositories;
+using POS.Models.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointOfSalesSystem.Controllers
+{
+    public class EmployeeInfoController : Controller
+    {
+        BranchManager _branchManager = new BranchManager();
+        EmployeeInfoManager _employeeInfoManager = new EmployeeInfoManager();
+        // GET: EmployeeInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EmployeeInfo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeInfo/Create
+        public ActionResult Create()
+        {
+            var model = new EmployeeInfoCreateVM();
+            model.Branches = _branchManager.GetAll();
+            model.EmployeeInfoses = _employeeInfoManager.GetAll();
+            return View(model);
+        }
+
+        // POST: EmployeeInfo/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeInfoCreateVM model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var EmployeeInfo = Mapper.Map<EmployeeInfo>(model);
+                    bool IsSaved = _employeeInfoManager.Save(EmployeeInfo);
+                    if(IsSaved)
+                    {
+                        RedirectToAction("Create");
+                    }
+                }
+                
+                return RedirectToAction("Create");
+            }
+            catch(Exception exception)
+            {
+                ModelState.AddModelError("", exception.Message);
+                model.Branches = _branchManager.GetAll();
+                model.EmployeeInfoses = _employeeInfoManager.GetAll();
+                return View(model);
+            }
+        }
+
+        // GET: EmployeeInfo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeInfo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeInfo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeInfo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PointOfSalesSystem/Models/ViewModels/EmployeeInfoCreateVM.cs
@@ -0,0 +1,37 @@
+﻿using POS.Models.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PointOfSalesSystem.Models.ViewModels
+{
+    public class EmployeeInfoCreateVM
+    {
+        public int Id { get; set; }
+        //Personal Info
+        public string EmergencyContactNo { get; set; }
+        public string NationalId { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string PermanentAddress { get; set; }
+        public string PresentAddress { get; set; }
+
+        //Official Info
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string ContactNo { get; set; }
+        public string JoiningDate { get; set; }
+        public string PersonalContactNo { get; set; }
+        public string Email { get; set; }
+        public byte[] Image { get; set; }
+
+        public int? EmployeeInfosId { get; set; }
+
+        public int? BranchId { get; set; }
+        public List<Branch> Branches { get; set; }
+
+        public List<EmployeeInfo> EmployeeInfoses { get; set; }
+    }
+}
