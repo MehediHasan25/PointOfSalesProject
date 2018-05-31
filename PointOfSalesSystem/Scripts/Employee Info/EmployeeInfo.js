dd26e27d6588115f322a8d339c76d2b33f0c890a
MehediHasan25 <ishanmehadi@gmail.com>
2018-05-31 16:37:36 +0600

    Employee info li hide show problem solved

---
+++ PointOfSalesSystem/Scripts/Employee Info/EmployeeInfo.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#per').hide();
+
+    $("#personal").on('click', function () {
+        $('#per').show();
+        $('#off'). hide();
+    });
+
+    $("#official").on('click', function () {
+
+        $("#off"). show();
+        $("#per"). hide();
+    });
+    
+});
